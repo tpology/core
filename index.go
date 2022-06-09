e2425aac331f4cca31b6937a9b6df2292e12b4cb
Scott Long <smxlong@gmail.com>
2022-06-08 17:22:26 -0700

    Error checkig (#5)
    * better add and remove validation
    
    * validate name uniqueness

--- index.go
+++ index.go
@@ -27,41 +27,63 @@ func NewIndex() *Index {
 }
 
 // AddResource adds a resource to the index
-func (i *Index) AddResource(r *v1.Resource) {
+func (i *Index) AddResource(r *v1.Resource) error {
 	if _, ok := i.resourceByKind[r.Resource.Kind]; !ok {
 		i.resourceByKind[r.Resource.Kind] = map[string]*v1.Resource{}
 	}
+	if _, ok := i.resourceByKind[r.Resource.Kind][r.Resource.Name]; ok {
+		return fmt.Errorf("resource %s of kind %s already exists", r.Resource.Name, r.Resource.Kind)
+	}
 	i.resourceByKind[r.Resource.Kind][r.Resource.Name] = r
+	return nil
 }
 
 // RemoveResource removes a resource from the index
-func (i *Index) RemoveResource(r *v1.Resource) {
+func (i *Index) RemoveResource(r *v1.Resource) error {
 	if _, ok := i.resourceByKind[r.Resource.Kind]; ok {
 		delete(i.resourceByKind[r.Resource.Kind], r.Resource.Name)
 		if len(i.resourceByKind[r.Resource.Kind]) == 0 {
 			delete(i.resourceByKind, r.Resource.Kind)
 		}
+		return nil
 	}
+	return fmt.Errorf("resource %s of kind %s does not exist", r.Resource.Name, r.Resource.Kind)
 }
 
 // AddTemplate adds a template to the index
-func (i *Index) AddTemplate(t *v1.Template) {
+func (i *Index) AddTemplate(t *v1.Template) error {
+	if _, ok := i.template[t.Template.Name]; ok {
+		return fmt.Errorf("template %s already exists", t.Template.Name)
+	}
 	i.template[t.Template.Name] = t
+	return nil
 }
 
 // RemoveTemplate removes a template from the index
-func (i *Index) RemoveTemplate(t *v1.Template) {
-	delete(i.template, t.Template.Name)
+func (i *Index) RemoveTemplate(t *v1.Template) error {
+	if _, ok := i.template[t.Template.Name]; ok {
+		delete(i.template, t.Template.Name)
+		return nil
+	}
+	return fmt.Errorf("template %s does not exist", t.Template.Name)
 }
 
 // AddRepository adds a repository to the index
-func (i *Index) AddRepository(r *v1.Repository) {
+func (i *Index) AddRepository(r *v1.Repository) error {
+	if _, ok := i.repository[r.Repository.Name]; ok {
+		return fmt.Errorf("repository %s already exists", r.Repository.Name)
+	}
 	i.repository[r.Repository.Name] = r
+	return nil
 }
 
 // RemoveRepository removes a repository from the index
-func (i *Index) RemoveRepository(r *v1.Repository) {
-	delete(i.repository, r.Repository.Name)
+func (i *Index) RemoveRepository(r *v1.Repository) error {
+	if _, ok := i.repository[r.Repository.Name]; ok {
+		delete(i.repository, r.Repository.Name)
+		return nil
+	}
+	return fmt.Errorf("repository %s does not exist", r.Repository.Name)
 }
 
 func (i *Index) Load(dir string) []error {
@@ -128,10 +150,16 @@ func (i *Index) Load(dir string) []error {
 			var resource v1.Resource
 			err = yaml.Unmarshal(yamlBytes, &resource)
 			if err != nil {
-				errs = append(errs, err)
+				// Format the error to prepend the resource path
+				errs = append(errs, fmt.Errorf("%s: %s", path, err))
 				return nil
 			}
-			i.AddResource(&resource)
+			err = i.AddResource(&resource)
+			if err != nil {
+				// Format the error to prepend the resource path
+				errs = append(errs, fmt.Errorf("%s: %s", path, err))
+				return nil
+			}
 		} else if _, ok := doc["template"]; ok {
 			// If there is a template key, unmarshal as Template
 			verrs := validateTemplateFields(doc)
@@ -153,10 +181,16 @@ func (i *Index) Load(dir string) []error {
 			var template v1.Template
 			err = yaml.Unmarshal(yamlBytes, &template)
 			if err != nil {
-				errs = append(errs, err)
+				// Format the error to prepend the resource path
+				errs = append(errs, fmt.Errorf("%s: %s", path, err))
 				return nil
 			}
-			i.AddTemplate(&template)
+			err = i.AddTemplate(&template)
+			if err != nil {
+				// Format the error to prepend the resource path
+				errs = append(errs, fmt.Errorf("%s: %s", path, err))
+				return nil
+			}
 		} else if _, ok := doc["repository"]; ok {
 			// If there is a repository key, unmarshal as Repository
 			verrs := validateRepositoryFields(doc)
@@ -178,10 +212,16 @@ func (i *Index) Load(dir string) []error {
 			var repository v1.Repository
 			err = yaml.Unmarshal(yamlBytes, &repository)
 			if err != nil {
-				errs = append(errs, err)
+				// Format the error to prepend the resource path
+				errs = append(errs, fmt.Errorf("%s: %s", path, err))
 				return nil
 			}
-			i.AddRepository(&repository)
+			err = i.AddRepository(&repository)
+			if err != nil {
+				// Format the error to prepend the resource path
+				errs = append(errs, fmt.Errorf("%s: %s", path, err))
+				return nil
+			}
 		} else {
 			errs = append(errs, fmt.Errorf("%s: no resource or template", path))
 		}

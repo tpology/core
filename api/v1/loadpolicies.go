106154a600d92f7c4ba694804dcfb4401f9fcd27
Scott Long <smxlong@gmail.com>
2022-06-09 03:14:08 +0000

    adding loadpolicies

---
+++ api/v1/loadpolicies.go
@@ -0,0 +1,33 @@
+package v1
+
+// Effect is the effect of a policy rule.
+type Effect string
+
+// Deny is the effect of a policy rule that denies access.
+const Deny Effect = "deny"
+
+// Allow is the effect of a policy rule that allows access.
+const Allow Effect = "allow"
+
+// LoadPolicy represents a load policy which allows or denies a resource to
+// be loaded from the directory structure based on its location and contents.
+type LoadPolicy struct {
+	// Name is the name of the load policy.
+	Name string `yaml:"name"`
+	// Effect is the effect of the load policy.
+	Effect string `yaml:"effect"`
+	// Path is the path pattern of the resources covered by the load policy.
+	Path string `yaml:"path"`
+	// Resource is the spec of a resource covered by the policy. All non-empty
+	// fields of this resource must match the actual resource being loaded for
+	// the policy to match.
+	Resource *ResourceSpec `yaml:"resource"`
+	// Template is the spec of a template covered by the policy. All non-empty
+	// fields of this template must match the actual template being loaded for
+	// the policy to match.
+	Template *TemplateSpec `yaml:"template"`
+	// Repository is the spec of a repository covered by the policy. All non-empty
+	// fields of this repository must match the actual repository being loaded for
+	// the policy to match.
+	Repository *RepositorySpec `yaml:"repository"`
+}
